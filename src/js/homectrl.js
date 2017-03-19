458c6f29672f98e6935027db6a6d38e988cdab3c
hatch514 <hr.katagiri@gmail.com>
2017-03-19 23:47:13 +0900

    [#1] 3*3 game screen

--- src/js/homectrl.js
+++ src/js/homectrl.js
@@ -1,6 +1,44 @@
 angular.module('256App',[])
-  .controller('homeController',function(){
-    this.init = function(){
-      console.log("init!");
-    };
-});
+  .controller('gameController', gameController);
+
+function gameController($scope){
+
+  $scope.ary0;
+  $scope.ary1;
+  $scope.ary2;
+  $scope.allSheets;
+
+  class SheetObj {
+    constructor(){
+      this.hasBlock = false;
+    }
+    
+    getBlock(blockObj){
+      this.block = blockObj;
+      this.hasBlock = true;
+    }
+  }
+
+  class BlockObj {
+    constructor(point, coord){ 
+      this.point = point;
+      this.coord = coord;
+    }
+  }
+
+  $scope.init = function(){
+    $scope.initializeBlock();
+    $scope.createBlock();
+  };
+
+  $scope.initializeBlock = function(){
+    $scope.ary0 = [new SheetObj(), new SheetObj(), new SheetObj()];
+    $scope.ary1 = [new SheetObj(), new SheetObj(), new SheetObj()];
+    $scope.ary2 = [new SheetObj(), new SheetObj(), new SheetObj()];
+    $scope.sheetTable = [$scope.ary0, $scope.ary1, $scope.ary2];
+  }
+
+  $scope.createBlock = function(){
+    $scope.sheetTable[0][0].getBlock(new BlockObj(2, [0, 0]));
+  }
+}
