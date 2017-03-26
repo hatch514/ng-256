3070ad9ea52352e310d3ed085a9eeb6969428721
hatch514 <hr.katagiri@gmail.com>
2017-03-26 15:49:15 +0900

    [#4] create one block every one turn

--- src/js/homectrl.js
+++ src/js/homectrl.js
@@ -7,30 +7,25 @@ function gameController($scope){
   const KEY_DOWN = 40;
   const KEY_LEFT = 37;
 
-  const DIR_UP = "up";
-  const DIR_RIGHT = "right";
-  const DIR_DOWN = "down";
-  const DIR_LEFT = "left";
- 
   const EDGE_TOP  = 0;
   const EDGE_RIGHT = 2;
   const EDGE_BOTTOM = 2;
   const EDGE_LEFT = 0;
  
   const ANIMATION_CSS = { 
-    up:[
+    '38':[                // KEY UP
       {bottom: '+=130'},
       {bottom: '+=260'}
     ],
-    right:[
+    '39':[                // KEY RIGHT
       {left: '+=130'},
       {left: '+=260'}
     ],
-    down:[
+    '40':[                // KEY DOWN 
       {top: '+=130'},
       {top: '+=260'}
     ],
-    left:[
+    '37':[                // KEY LEFT 
       {right: '+=130'}, 
       {right: '+=260'}
     ]
@@ -44,7 +39,9 @@ function gameController($scope){
   $scope.sheetTable;
   $scope.allBlocks;
 
-  class SheetObj {
+  self = this;
+
+  class sheetObj {
     constructor(){
       this.hasBlock = false;
     }
@@ -64,7 +61,7 @@ function gameController($scope){
     }
   }
 
-  class BlockObj {
+  class blockObj {
     constructor(point, coords){ 
       this.point = point;
       this.coords = coords;
@@ -73,13 +70,15 @@ function gameController($scope){
     
     setCoords(coords){
       this.coords = coords;
+      return new Promise(function(callback){callback()});
     }
 
     setAnimation(direction, hop, callback){
       if(hop>=0){
         $('.block').animate(
           ANIMATION_CSS[direction][hop-1], 
-          ANIMATION_DURATION, ANIMATION_EASING, callback);
+          ANIMATION_DURATION, ANIMATION_EASING, callback
+        );
       }
     }
   }
@@ -87,37 +86,39 @@ function gameController($scope){
   function getRandomInt(max,min){
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * 3);
+    return Math.floor(Math.random() * (max - min));
   }
 
   function initializeSheets(){
-    $scope.ary0 = [new SheetObj(), new SheetObj(), new SheetObj()];
-    $scope.ary1 = [new SheetObj(), new SheetObj(), new SheetObj()];
-    $scope.ary2 = [new SheetObj(), new SheetObj(), new SheetObj()];
+    $scope.ary0 = [new sheetObj(), new sheetObj(), new sheetObj()];
+    $scope.ary1 = [new sheetObj(), new sheetObj(), new sheetObj()];
+    $scope.ary2 = [new sheetObj(), new sheetObj(), new sheetObj()];
     $scope.sheetTable = [$scope.ary0, $scope.ary1, $scope.ary2];
     $scope.allBlocks = [];
   }
 
   function createBlock(){
+    console.log('called');
     var coordsList = [];
     for(var i=0; i<(EDGE_RIGHT+1); i++){
       for(var j=0; j<(EDGE_RIGHT+1); j++){
-        coordsList.push({x:i, y:j});
+        coordsList.push({x:j, y:i});
       }
     }
     $scope.allBlocks.forEach(function(block){
       var block_x = block.coords.x;
       var block_y = block.coords.y;
-      var index = null;
+      var deleteIndex = null;
       coordsList.forEach(function(coords,index){
-        if(block_x == coords.x && block_y == coords.y) this.index = index;
+        if(block_x == coords.x && block_y == coords.y) deleteIndex = index;
       });
-      if(index) coordsList.split(index,1); 
-     
+      if(deleteIndex) {
+        coordsList.splice(deleteIndex,1); 
+      }    
     }); 
     
-    var createIndex = getRandomInt(coordsList.length + 1);
-    var newBlock = new BlockObj(2, coordsList[createIndex]);
+    var createIndex = getRandomInt(coordsList.length, 0) ;
+    var newBlock = new blockObj(2, coordsList[createIndex]);
 
     $scope.allBlocks.push(newBlock);
     $scope.sheetTable[newBlock.coords.y][newBlock.coords.x].setBlock(newBlock);
@@ -141,11 +142,75 @@ function gameController($scope){
     var key = event.which;
     if(key==KEY_UP || key==KEY_RIGHT || key==KEY_DOWN || key==KEY_LEFT){
       moveBlocks(key);
-      createBlock();
     }
   }
 
+  function deepCopy(oldObject){
+    var newObject = jQuery.extend(true, {}, oldObject);
+    return newObject;
+  }
+  
+  function sortAllBlocks(direction){
+    tempAllBlocks = [];
+
+    if(direction==KEY_UP){
+      for(var y = 0; y <= EDGE_BOTTOM; y++){
+        for(var x = 0; x <= EDGE_RIGHT; x++){
+          $scope.allBlocks.forEach(function(block){
+            if(block.coords.x == x && block.coords.y == y){
+              //use deep copy
+              tempAllBlocks.push(block);
+            } 
+          });
+        }
+      }
+    }else if(direction==KEY_RIGHT){
+      for(var x = EDGE_RIGHT; x >= 0; x--){
+        for(var y = 0; y <= EDGE_BOTTOM; y++){
+          $scope.allBlocks.forEach(function(block){
+            if(block.coords.x == x && block.coords.y == y){
+              tempAllBlocks.push(block);
+            } 
+          });
+        }
+      }
+    }else if(direction==KEY_DOWN){
+      for(var y = EDGE_BOTTOM; y >= 0 ; y--){
+        for(var x = 0; x <= EDGE_RIGHT; x++){
+          $scope.allBlocks.forEach(function(block){
+            if(block.coords.x == x && block.coords.y == y){
+              tempAllBlocks.push(block);
+            } 
+          });
+        }
+      }
+    }else if(direction==KEY_LEFT){
+      for(var x = 0; x <= EDGE_RIGHT; x++){
+        for(var y = 0; y <= EDGE_BOTTOM; y++){
+          $scope.allBlocks.forEach(function(block){
+            if(block.coords.x == x && block.coords.y == y){
+              tempAllBlocks.push(block);
+            } 
+          });
+        }
+      }
+    }
+    $scope.allBlocks = tempAllBlocks; 
+
+  }
+  
+  //change logic fundamentally
   function moveBlocks(direction){
+    sortAllBlocks(direction);
+    blockAnimate(direction).then(function(){
+      createBlock(); 
+      return;
+    }).then(function(){
+      $scope.$digest();
+    });
+  }
+
+  function blockAnimate(direction){
     $scope.allBlocks.forEach(function(block){
       var now_x = block.coords.x;
       var now_y = block.coords.y;
@@ -154,58 +219,36 @@ function gameController($scope){
       var next_x, next_y;
       var nextSheet; 
       var hop; 
+      
       if(direction == KEY_UP){
         next_x = now_x;
         next_y = EDGE_TOP;
         nextSheet = $scope.sheetTable[next_y][next_x];
         hop = now_y - EDGE_TOP; 
-        
-        block.setAnimation(DIR_UP, hop, function(){
-          nowSheet.deleteBlock();
-          block.setCoords({x:next_x, y:next_y});
-          nextSheet.setBlock(block);
-          $scope.$digest();
-        });
-
       }else if(direction==KEY_RIGHT){
         next_x = EDGE_RIGHT;
         next_y = now_y;
         nextSheet = $scope.sheetTable[next_y][next_x];
         hop = EDGE_RIGHT - now_x; 
-        
-        block.setAnimation(DIR_RIGHT, hop, function(){
-          nowSheet.deleteBlock();
-          block.setCoords({x:next_x, y:next_y});
-          nextSheet.setBlock(block);
-          $scope.$digest();
-        });
-
       }else if(direction==KEY_DOWN){
         next_x = now_x;
         next_y = EDGE_BOTTOM;
         nextSheet = $scope.sheetTable[next_y][next_x];
         hop = EDGE_BOTTOM - now_y; 
-
-        block.setAnimation(DIR_DOWN, hop, function(){
-          nowSheet.deleteBlock();
-          block.setCoords({x:next_x, y:next_y});
-          nextSheet.setBlock(block);
-          $scope.$digest();
-        });
-
       }else if(direction==KEY_LEFT){
         next_x = EDGE_LEFT;
         next_y = now_y;
         nextSheet = $scope.sheetTable[next_y][next_x];
         hop = now_x - EDGE_LEFT; 
-        
-        block.setAnimation(DIR_LEFT, hop, function(){
-          nowSheet.deleteBlock();
-          block.setCoords({x:next_x, y:next_y});
+      }
+
+      block.setAnimation(direction, hop, function(){
+        nowSheet.deleteBlock();
+        block.setCoords({x:next_x, y:next_y}).then(function(){
           nextSheet.setBlock(block);
-          $scope.$digest();
         });
-       }
+      });
     });
+    return new Promise(function(callback){callback();}); 
   }
 }
